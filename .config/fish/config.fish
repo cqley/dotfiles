4b4a4c4b5d1fddc37af48fa7c73ee02b37472dd9
silly <u65487754@gmail.com>
2026-02-08 10:45:03 +0100

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,6 @@
 function fish_greeting
 
 if status is-interactive
-    # You only need this if you want to use the color variables
-    # (like $color1) in your fish prompt/functions
     [ -f ~/.cache/wal/colors-fish.conf ]; and source ~/.cache/wal/colors-fish.conf
 end
 
