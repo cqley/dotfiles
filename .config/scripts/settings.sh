daf0b4b840bf32c02dd1baae93be80adb89a0782
silly <u65487754@gmail.com>
2026-03-07 14:32:16 +0100

    Implement toggle for Hyprland corners settings
    Add script to toggle Hyprland decoration settings.

--- .config/scripts/settings.sh
+++ .config/scripts/settings.sh
@@ -1,1 +1,31 @@
+#!/bin/bash
+HYPR_CONF="$HOME/.config/hypr/hyprland.conf"
 
+options="󰃟 sharp/round toggle"
+choice=$(echo -e "$options" | rofi -dmenu -i -p "hyprland settings")
+
+[[ -z "$choice" ]] && exit
+
+case "$choice" in
+    *Corners*)
+        if grep -q "rounding = 0 # @dynamic_rounding" "$HYPR_CONF"; then
+            sed -i '/@dynamic_rounding/c\    rounding = 15 # @dynamic_rounding' "$HYPR_CONF"
+            sed -i '/@dynamic_power/c\    rounding_power = 10 # @dynamic_power' "$HYPR_CONF"
+            sed -i '/@dynamic_smartgaps/s/^\([^#]\)/#\1/' "$HYPR_CONF"
+            
+            hyprctl keyword decoration:rounding 15 > /dev/null
+            hyprctl keyword decoration:rounding_power 10 > /dev/null
+            hyprctl reload > /dev/null
+            notify-send "toggled" "rounded"
+        else
+            sed -i '/@dynamic_rounding/c\    rounding = 0 # @dynamic_rounding' "$HYPR_CONF"
+            sed -i '/@dynamic_power/c\    rounding_power = 0 # @dynamic_power' "$HYPR_CONF"
+            sed -i '/@dynamic_smartgaps/s/^#//g' "$HYPR_CONF"
+            
+            hyprctl keyword decoration:rounding 0 > /dev/null
+            hyprctl keyword decoration:rounding_power 0 > /dev/null
+            hyprctl reload > /dev/null
+            notify-send "toggled" "sharp"
+        fi
+        ;;
+esac
