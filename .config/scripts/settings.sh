18a347f6c112b6198afe6605575fa7d7bfbf27f4
silly <u65487754@gmail.com>
2026-03-07 14:34:13 +0100

    Update settings.sh

--- .config/scripts/settings.sh
+++ .config/scripts/settings.sh
@@ -7,12 +7,12 @@ choice=$(echo -e "$options" | rofi -dmenu -i -p "hyprland settings")
 [[ -z "$choice" ]] && exit
 
 case "$choice" in
-    *Corners*)
+    *toggle*)
         if grep -q "rounding = 0 # @dynamic_rounding" "$HYPR_CONF"; then
             sed -i '/@dynamic_rounding/c\    rounding = 15 # @dynamic_rounding' "$HYPR_CONF"
             sed -i '/@dynamic_power/c\    rounding_power = 10 # @dynamic_power' "$HYPR_CONF"
             sed -i '/@dynamic_smartgaps/s/^\([^#]\)/#\1/' "$HYPR_CONF"
-            
+
             hyprctl keyword decoration:rounding 15 > /dev/null
             hyprctl keyword decoration:rounding_power 10 > /dev/null
             hyprctl reload > /dev/null
@@ -21,7 +21,7 @@ case "$choice" in
             sed -i '/@dynamic_rounding/c\    rounding = 0 # @dynamic_rounding' "$HYPR_CONF"
             sed -i '/@dynamic_power/c\    rounding_power = 0 # @dynamic_power' "$HYPR_CONF"
             sed -i '/@dynamic_smartgaps/s/^#//g' "$HYPR_CONF"
-            
+
             hyprctl keyword decoration:rounding 0 > /dev/null
             hyprctl keyword decoration:rounding_power 0 > /dev/null
             hyprctl reload > /dev/null
