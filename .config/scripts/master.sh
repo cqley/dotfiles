2f9d5b584dbbddb4a9356de721aaae419b3bc475
silly <u65487754@gmail.com>
2026-03-07 15:33:24 +0100

    Update master.sh

--- .config/scripts/master.sh
+++ .config/scripts/master.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+SCRIPT_DIR="$HOME/.config/scripts"
 
+options="󰸉 wallpaper\n󰳗 decorations\n󰒓 fastfetch"
+
+choice=$(echo -e "$options" | rofi -dmenu -i -p "master" -theme-str 'window {width: 400px;}')
+
+case "$choice" in
+    *wallpaper*) "$SCRIPT_DIR/wallpaper.sh" ;;
+    *decorations*) "$SCRIPT_DIR/settings.sh" ;;
+    *fastfetch*) kitty sh -c "fastfetch; read -p 'press enter to close...'" ;;
+esac
