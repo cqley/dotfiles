b87bdd4e04a17d7802b7d0c6ba089382efbcd50a
silly <u65487754@gmail.com>
2026-03-08 13:06:50 +0100

    Update settings.sh

--- .config/scripts/settings.sh
+++ .config/scripts/settings.sh
@@ -14,13 +14,19 @@ case "$choice" in
             sed -i '/@dynamic_rounding/c\    rounding = 15 # @dynamic_rounding' "$HYPR_CONF"
             sed -i '/@dynamic_power/c\    rounding_power = 10 # @dynamic_power' "$HYPR_CONF"
             sed -i '/@dynamic_dunst/c\    corner_radius = 15 # @dynamic_dunst' "$DUNST_CONF"
+            sed -i '/@dynamic_smartgaps/s/^\([^#]\)/#\1/' "$HYPR_CONF"
+
             hyprctl keyword decoration:rounding 15 > /dev/null
+            hyprctl reload > /dev/null
             notify-send "toggled" "rounded"
         else
             sed -i '/@dynamic_rounding/c\    rounding = 0 # @dynamic_rounding' "$HYPR_CONF"
             sed -i '/@dynamic_power/c\    rounding_power = 0 # @dynamic_power' "$HYPR_CONF"
             sed -i '/@dynamic_dunst/c\    corner_radius = 0 # @dynamic_dunst' "$DUNST_CONF"
+            sed -i '/@dynamic_smartgaps/s/^#//g' "$HYPR_CONF"
+
             hyprctl keyword decoration:rounding 0 > /dev/null
+            hyprctl reload > /dev/null
             notify-send "toggled" "sharp"
         fi
         killall dunst && dunst &
