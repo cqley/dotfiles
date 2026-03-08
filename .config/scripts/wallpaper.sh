bbbd0a3c72187c44ee862cd77b7de29437456d14
silly <u65487754@gmail.com>
2026-03-08 04:16:08 +0100

    Update wallpaper.sh

--- .config/scripts/wallpaper.sh
+++ .config/scripts/wallpaper.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+WALL_DIR="$HOME/.config/wallpaper"
+
+selected=$(ls "$WALL_DIR" | fuzzel -d -p "> ")
+[[ -z "$selected" ]] && exit
+FULL_PATH="$WALL_DIR/$selected"
+
+swww img "$FULL_PATH" --transition-type grow --transition-duration 1.5 --transition-fps 120
+
+wal -i "$FULL_PATH" -n -q
+
+kill -SIGUSR1 $(pgrep kitty)
+
+killall dunst
+dunst &
+
+sleep 0.1
+notify-send "theme" "updated from $selected"
