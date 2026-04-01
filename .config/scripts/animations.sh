0cd42af6ef8cb24b3ec24d7525ac302a55cf21f6
silly <u65487754@gmail.com>
2026-04-01 03:17:08 +0200

    Update animations.sh

--- .config/scripts/animations.sh
+++ .config/scripts/animations.sh
@@ -13,20 +13,20 @@ case "$choice" in
         sed -i '/@dynamic_special/c\    animation = specialWorkspace, 1, 1.94, almostLinear, fade # @dynamic_special' "$HYPR_CONF"
         hyprctl keyword animation "workspaces, 1, 1.94, almostLinear, fade"
         hyprctl keyword animation "specialWorkspace, 1, 1.94, almostLinear, fade"
-        notify-send "animations" "fade mode"
+        notify-send "animations" "fade"
         ;;
     vertical)
         sed -i '/@dynamic_workspaces/c\    animation = workspaces, 1, 5, hard, slidevert # @dynamic_workspaces' "$HYPR_CONF"
         sed -i '/@dynamic_special/c\    animation = specialWorkspace, 1, 5, hard, slidevert # @dynamic_special' "$HYPR_CONF"
         hyprctl keyword animation "workspaces, 1, 5, hard, slidevert"
         hyprctl keyword animation "specialWorkspace, 1, 5, hard, slidevert"
-        notify-send "animations" "vertical slide"
+        notify-send "animations" "vertical"
         ;;
     horizontal)
         sed -i '/@dynamic_workspaces/c\    animation = workspaces, 1, 5, hard, slide # @dynamic_workspaces' "$HYPR_CONF"
         sed -i '/@dynamic_special/c\    animation = specialWorkspace, 1, 5, hard, slide # @dynamic_special' "$HYPR_CONF"
         hyprctl keyword animation "workspaces, 1, 5, hard, slide"
         hyprctl keyword animation "specialWorkspace, 1, 5, hard, slide"
-        notify-send "animations" "horizontal slide"
+        notify-send "animations" "horizontal"
         ;;
 esac
