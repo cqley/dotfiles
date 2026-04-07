fd1fbc34c528101eb3aa763240255ba1b471f146
silly <u65487754@gmail.com>
2026-04-07 05:22:22 +0000

    Update master.sh

--- .config/scripts/master.sh
+++ .config/scripts/master.sh
@@ -2,12 +2,13 @@
 
 SCRIPT_DIR="$HOME/.config/scripts"
 
-options="btop\nconfig\nsettings\nwallpaper\nssh"
+options="btop\nnetwork\nconfig\nsettings\nwallpaper\nssh"
 
 choice=$(echo -e "$options" | rofi -dmenu -i -p ">")
 
 case "$choice" in
     *btop*) kitty btop ;;
+    *network*) "$SCRIPT_DIR/network.sh" ;;
     *config*) "$SCRIPT_DIR/config.sh" ;;
     *settings*) "$SCRIPT_DIR/settings.sh" ;;
     *wallpaper*) "$SCRIPT_DIR/wallpaper.sh" ;;
