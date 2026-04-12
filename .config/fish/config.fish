28b5e82fe6b21be3f01726fcf2292139211cb758
silly <u65487754@gmail.com>
2026-04-12 07:35:56 +0000

    Add abbreviations for paru package manager commands

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,6 +10,8 @@ abbr -a s sudo
 abbr -a update sudo pacman -Syu
 abbr -a pi sudo pacman -S
 abbr -a pr sudo pacman -Rns
+abbr -a pi paru -S
+abbr -a ps paru -Ss
 abbr -a yi yay -S
 abbr -a ys yay -Ss
 abbr -a zed zeditor
