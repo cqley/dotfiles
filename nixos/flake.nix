586bd3baadb5c54c6d55164f95e7ba257f5f5db9
caley <u65487754@gmail.com>
2026-05-20 01:19:18 +0000

    Create flake.nix

---
+++ nixos/flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "cat";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations = {
+      cat = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
