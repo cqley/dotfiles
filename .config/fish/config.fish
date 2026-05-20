93b542dc450e38f02542f99b612063c1b2750f08
caley <u65487754@gmail.com>
2026-05-20 07:40:26 +0000

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,13 +7,7 @@ function fish_prompt
 end
 
 abbr -a s sudo
-abbr -a update sudo pacman -Syu
-abbr -a pi sudo pacman -S
-abbr -a pr sudo pacman -Rns
-abbr -a pp paru -S
-abbr -a ps paru -Ss
-abbr -a yi yay -S
-abbr -a ys yay -Ss
+abbr -a update
 abbr -a zed zeditor
 
 if status is-interactive
