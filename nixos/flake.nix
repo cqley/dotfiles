2b330d276db7f655e85bec19e5102e88f001a1aa
caley <u65487754@gmail.com>
2026-06-02 15:31:28 +0200

    Update flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,26 +3,29 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    helium = {
+      url = "github:AlvaroParker/helium-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, helium, ... }@inputs: {
     nixosConfigurations = {
       cat = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
         modules = [
           ./configuration.nix
-          
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            
             home-manager.users.silly = import ./cat.nix;
+            home-manager.extraSpecialArgs = { inherit inputs; };
           }
         ];
       };
