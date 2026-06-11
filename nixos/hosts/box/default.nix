5a50c439e0bd5f23d3e6245690e63798c18f2dbc
caley <u65487754@gmail.com>
2026-06-11 23:07:58 +0200

    Update default.nix

--- nixos/hosts/box/default.nix
+++ nixos/hosts/box/default.nix
@@ -1,1 +1,84 @@
+{ config, pkgs, ... }: {
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
+  networking.hostName = "box";
+  networking.networkmanager.enable = true;
+  users.users.cat.extraGroups = [ "networkmanager" ];
+
+  boot.loader.grub = {
+    enable = true;
+    device = "/dev/nvme0n1";
+    useOSProber = true;
+  };
+
+  zramSwap = {
+    enable = true;
+    memoryPercent = 25;
+  };
+
+  services.getty.autologinUser = "cat";
+  services.fstrim.enable = true;
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    nvidiaSettings = true;
+    open = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  programs.hyprland.enable = true;
+  programs.dconf.enable = true;
+  programs.steam.enable = true;
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    kitty
+    wl-clipboard
+    awww
+    libnotify
+    pywal
+    quickshell
+    grim
+    slurp
+    kdePackages.dolphin
+    imv
+    mpv
+    obs-studio
+    prismlauncher
+    obsidian
+  ];
+
+  fonts.packages = with pkgs; [
+    corefonts
+    noto-fonts
+    noto-fonts-cjk-sans
+    google-fonts
+    nerd-fonts.jetbrains-mono
+    noto-fonts-color-emoji
+    twemoji-color-font
+  ];
+
+  system.stateVersion = "26.05";
+}
