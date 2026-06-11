27be0670e3a4854c9fd5b426e7e627f3cbbc2cb5
caley <u65487754@gmail.com>
2026-06-11 23:20:28 +0200

    Add NixOS configuration for SSH and Pufferpanel

--- nixos/hosts/bin/default.nix
+++ nixos/hosts/bin/default.nix
@@ -1,1 +1,18 @@
+{ ... }: {
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
+  networking.hostName = "bin";
+  
+  services.openssh.enable = true;
+  services.pufferpanel.enable = true;
+
+  networking.firewall.allowedTCPPorts = [ 22 8080 5657 ];
+
+  users.users.cat.openssh.authorizedKeys.keys = [
+    ""
+  ];
+
+  system.stateVersion = "26.05";
+}
