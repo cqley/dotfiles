9432c0d987089fe654b12a02c4d4e36d5552701c
caley <u65487754@gmail.com>
2026-06-11 22:51:02 +0200

    Update flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -12,16 +12,40 @@
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations = {
-      litterbox = nixpkgs.lib.nixosSystem {
+      box = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          ./configuration.nix
-          
+          ./common.nix
+          ./hosts/box
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.cat = import ./mngr.nix;
+            home-manager.users.cat = {
+              imports = [
+                ./mngr.nix
+                ./hypr.nix
+                ./shell.nix
+              ];
+            };
+          }
+        ];
+      };
+
+      bin = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./common.nix
+          ./hosts/bin
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.cat = {
+              imports = [
+                ./mngr.nix
+              ];
+            };
           }
         ];
       };
