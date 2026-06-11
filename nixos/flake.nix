82aeb6091351c57ca3c6b89593a5d02822201283
caley <u65487754@gmail.com>
2026-06-11 22:34:14 +0200

    Update flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "cat";
+  description = "litterbox";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -12,7 +12,7 @@
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations = {
-      cat = nixpkgs.lib.nixosSystem {
+      litterbox = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
@@ -21,7 +21,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.silly = import ./cat.nix;
+            home-manager.users.cat = import ./mngr.nix;
           }
         ];
       };
