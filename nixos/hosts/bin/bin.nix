06baf9eb1d05e8ce9f8999198ef8bc58c88ce771
caley <u65487754@gmail.com>
2026-06-15 15:35:53 +0200

    Add configuration for fish, kitty, and btop programs

--- nixos/hosts/bin/bin.nix
+++ nixos/hosts/bin/bin.nix
@@ -1,1 +1,154 @@
+{ config, pkgs, ... }:
 
+{
+  imports = [
+    ./mdls/vim.nix
+  ];
+
+  home.username = "cat";
+  home.homeDirectory = "/home/cat";
+  home.stateVersion = "26.05";
+
+  xdg.configFile."fish/config.fish".force = true;
+  xdg.configFile."kitty/kitty.conf".force = true;
+  xdg.configFile."btop/btop.conf".force = true;
+
+  programs.fish = {
+    enable = true;
+    shellAbbrs = {
+      rebuild = "sudo nixos-rebuild switch --flake /etc/nixos/#$hostname";
+      update = "nix flake update --flake /etc/nixos/ && sudo nixos-rebuild switch --flake /etc/nixos/#$hostname";
+    };
+    functions = {
+      fish_greeting = { body = ""; };
+      fish_prompt = {
+        body = ''
+          echo -n (prompt_pwd)
+          echo -n " > "
+        '';
+      };
+    };
+    interactiveShellInit = ''
+      if status is-interactive
+          cat ~/.cache/wal/sequences
+      end
+    '';
+    loginShellInit = ''
+      if test -z "$DISPLAY" -a (tty) = "/dev/tty1"
+        exec start-hyprland
+      end
+    '';
+  };
+
+  programs.kitty = {
+    enable = true;
+    extraConfig = ''
+      include ~/.cache/wal/colors-kitty.conf
+
+      shell fish
+      shell_integration enabled
+
+      font_family JetBrainsMono Nerd Font
+      font_size 11
+      
+      cursor_trail 3
+      cursor_trail_decay 0.1 0.4
+      cursor_shape beam
+      cursor_blink_interval 1
+      shell_integration no-cursor
+
+      confirm_os_window_close 0
+      allow_remote_control yes
+
+      background_opacity 1
+      background_blur 1
+      dynamic_background_opacity 1
+      scrollbar_handle_opacity 0
+      scrollbar_track_opacity 0
+      scrollbar_track_hover_opacity 0
+    '';
+  };
+
+  programs.btop = {
+    enable = true;
+    settings = {
+      color_theme = "TTY";
+      theme_background = true;
+      truecolor = true;
+      force_tty = false;
+      presets = "cpu:1:default,proc:0:default cpu:0:default,mem:0:default,net:0:default cpu:0:block,net:0:tty";
+      vim_keys = false;
+      rounded_corners = false;
+      terminal_sync = true;
+      update_ms = 2000;
+      background_update = true;
+      clock_format = "%X";
+      base_10_sizes = false;
+      base_10_bitrate = "Auto";
+      log_level = "WARNING";
+      save_config_on_exit = true;
+      shown_boxes = "cpu mem net proc";
+      graph_symbol = "braille";
+      graph_symbol_cpu = "default";
+      graph_symbol_gpu = "default";
+      graph_symbol_mem = "default";
+      graph_symbol_net = "default";
+      graph_symbol_proc = "default";
+      cpu_bottom = false;
+      cpu_graph_upper = "Auto";
+      cpu_graph_lower = "Auto";
+      cpu_invert_lower = true;
+      cpu_single_graph = false;
+      check_temp = true;
+      cpu_sensor = "Auto";
+      show_coretemp = true;
+      cpu_core_map = "";
+      temp_scale = "celsius";
+      show_cpu_freq = true;
+      freq_mode = "first";
+      show_cpu_watts = true;
+      show_uptime = true;
+      custom_cpu_name = "";
+      proc_left = false;
+      proc_sorting = "cpu lazy";
+      proc_reversed = false;
+      proc_tree = false;
+      proc_colors = true;
+      proc_gradient = true;
+      proc_per_core = false;
+      proc_mem_bytes = true;
+      proc_cpu_graphs = true;
+      proc_info_smaps = false;
+      proc_filter_kernel = false;
+      proc_aggregate = false;
+      keep_dead_proc_usage = false;
+      mem_graphs = true;
+      mem_below_net = false;
+      show_swap = true;
+      swap_disk = true;
+      zfs_arc_cached = true;
+      show_disks = true;
+      disks_filter = "";
+      only_physical = true;
+      use_fstab = true;
+      disk_free_priv = false;
+      show_io_stat = true;
+      io_mode = false;
+      io_graph_combined = false;
+      io_graph_speeds = "";
+      zfs_hide_datasets = false;
+      net_iface = "";
+      net_auto = true;
+      net_sync = true;
+      net_download = 100;
+      net_upload = 100;
+      show_gpu_info = "Auto";
+      shown_gpus = "nvidia";
+      gpu_mirror_graph = true;
+      nvml_measure_pcie_speeds = true;
+      rsmi_measure_pcie_speeds = true;
+    };
+  };
+
+  programs.home-manager.enable = true;
+}
