f35bcf662f1b1f84ea2be0e5794b940f6f38a518
caley <u65487754@gmail.com>
2026-06-20 16:11:40 +0200

    Update qksh.nix

--- nixos/mdls/qksh.nix
+++ nixos/mdls/qksh.nix
@@ -116,7 +116,7 @@
         ]
         property var masterCfgItems: [
             { label: "nix",   cmd: ["kitty", "-e", "sudo", "nvim", "/etc/nixos/hosts/box/configuration.nix"] },
-            { label: "mngr",  cmd: ["kitty", "-e", "sudo", "nvim", "/etc/nixos/hosts/box/box.nix"]           },
+            { label: "box",  cmd: ["kitty", "-e", "sudo", "nvim", "/etc/nixos/hosts/box/box.nix"]            },
             { label: "hypr",  cmd: ["kitty", "-e", "sudo", "nvim", "/etc/nixos/mdls/hypr.nix"]               },
             { label: "shell", cmd: ["kitty", "-e", "sudo", "nvim", "/etc/nixos/mdls/qksh.nix"]               },
             { label: "vim",   cmd: ["kitty", "-e", "sudo", "nvim", "/etc/nixos/mdls/vim.nix"]                }
