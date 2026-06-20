87044d300e312ba458a83c65a391173d390eece3
caley <u65487754@gmail.com>
2026-06-20 19:02:56 +0200

    Update box.nix

--- nixos/hosts/box/box.nix
+++ nixos/hosts/box/box.nix
@@ -19,8 +19,8 @@
   programs.fish = {
     enable = true;
     shellAbbrs = {
-      rebuild = "sudo nixos-rebuild switch --flake /etc/nixos/#$hostname";
-      update = "nix flake update --flake /etc/nixos/ && sudo nixos-rebuild switch --flake /etc/nixos/#$hostname";
+      rebuild = "sudo nixos-rebuild switch --flake /etc/nixos/#box";
+      update = "nix flake update --flake /etc/nixos/ && sudo nixos-rebuild switch --flake /etc/nixos/#box";
     };
     functions = {
       fish_greeting = { body = ""; };
