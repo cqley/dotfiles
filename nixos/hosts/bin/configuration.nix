4bd499ec6ec59ff1f87e6e3c42addce14d200096
caley <u65487754@gmail.com>
2026-06-23 11:22:09 +0200

    Update configuration.nix

--- nixos/hosts/bin/configuration.nix
+++ nixos/hosts/bin/configuration.nix
@@ -63,7 +63,10 @@
 
   services.pufferpanel = {
     enable = true;
-    extraPackages = [ pkgs.jdk25 ];
+    extraPackages = [
+      pkgs.jdk25
+      (pkgs.writeShellScriptBin "java25" "exec ${pkgs.jdk25}/bin/java \"$@\"")
+    ];
     environment = {
       PUFFER_WEB_HOST = ":8080";
       PUFFER_DAEMON_SFTP_HOST = ":5657";
