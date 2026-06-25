c302bbb5c976558d6ed07e84b907fb602efe8cbb
caley <u65487754@gmail.com>
2026-06-25 15:59:45 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,14 @@
-big overhaul for the install script soon, i wouldn't use it if i wasn't me tho <3
+#!/bin/sh
+dir=$(mktemp -d)
+git clone https://github.com/cqley/dotfiles "$dir"
+sudo rm -rf /etc/nixos
+sudo cp -r "$dir/nixos" /etc/
+rm -rf "$dir"
+
+sudo nixos-generate-config
+clear
+printf "host?\n"
+read -r host
+
+sudo mv /etc/nixos/hardware-configuration.nix "/etc/nixos/hosts/$host/"
+sudo rm /etc/nixos/configuration.nix
