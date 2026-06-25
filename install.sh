3e682f53b46835f40cdbddd0ab1e1c28c8fab115
caley <u65487754@gmail.com>
2026-06-25 16:02:54 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ rm -rf "$dir"
 sudo nixos-generate-config
 clear
 printf "host?\n"
-read -r host < /dev/tty
+read -r host
 
 sudo mv /etc/nixos/hardware-configuration.nix "/etc/nixos/hosts/$host/"
 sudo rm /etc/nixos/configuration.nix
