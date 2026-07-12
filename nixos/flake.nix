8045f4aeadd72ceddcdc7a6e52f71a7d91c84f6e
caley <195605706+cqley@users.noreply.github.com>
2026-07-12 13:37:46 +0200

    Update flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,23 +2,33 @@
   description = "litterbox";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }:
+  let
+    system = "x86_64-linux";
+    pkgs-unstable = import nixpkgs-unstable {
+      inherit system;
+      config.allowUnfree = true;
+    };
+  in {
     nixosConfigurations = {
       box = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+        inherit system;
+        specialArgs = { inherit pkgs-unstable; };
         modules = [
           ./hosts/box/configuration.nix
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit pkgs-unstable; };
             home-manager.users.cat = {
               imports = [ ./hosts/box/box.nix ./mdls/hypr.nix ./mdls/qksh.nix ./mdls/vi.nix ./mdls/colors.nix ./mdls/dayz.nix ];
             };
@@ -27,13 +37,15 @@
       };
 
       bin = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+        inherit system;
+        specialArgs = { inherit pkgs-unstable; };
         modules = [
           ./hosts/bin/configuration.nix
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit pkgs-unstable; };
             home-manager.users.cat = {
               imports = [ ./hosts/bin/bin.nix ./mdls/vi.nix ];
             };
