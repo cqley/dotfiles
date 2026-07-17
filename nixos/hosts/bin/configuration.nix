60a261dda6af404b2b81555c1e5ff68f67745ee6
caley <195605706+cqley@users.noreply.github.com>
2026-07-18 01:27:49 +0200

    Update configuration.nix

--- nixos/hosts/bin/configuration.nix
+++ nixos/hosts/bin/configuration.nix
@@ -111,6 +111,31 @@
     };
   };
 
+  services.cgit.blind = {
+    enable = true;
+    scanPath = "/srv/git";
+    nginx.virtualHost = "5.231.118.254";
+    gitHttpBackend = {
+      enable = true;
+      checkExportOkFiles = false;
+    };
+    settings = {
+      root-title = "blind";
+      root-desc = "";
+      enable-index-owner = 0;
+      enable-commit-graph = 1;
+      enable-log-filecount = 1;
+      enable-log-linecount = 1;
+      clone-prefix = "http://5.231.118.254";
+    };
+  };
+
+  services.nginx = {
+    enable = true;
+    virtualHosts."5.231.118.254" = {
+    };
+  };
+
   systemd.services.pocketbase = {
     wantedBy = [ "multi-user.target" ];
     after = [ "network.target" ];
