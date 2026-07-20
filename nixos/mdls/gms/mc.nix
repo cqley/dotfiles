ff60c405a042564675118714fbe36fd6907f7833
caley <195605706+cqley@users.noreply.github.com>
2026-07-21 01:07:22 +0200

    Update mc.nix

--- nixos/mdls/gms/mc.nix
+++ nixos/mdls/gms/mc.nix
@@ -34,7 +34,7 @@
         InstanceDir = "instances";
         JavaArchitecture = "64";
         JavaDir = "java";
-        JavaPath = /nix/store/55mw2ly3dxh5008li72rx6ilc75hgvr0-openjdk-8u502-b01/bin/java
+        JavaPath = "/nix/store/55mw2ly3dxh5008li72rx6ilc75hgvr0-openjdk-8u502-b01/bin/java";
         Language = "en_US";
         LastHostname = "box";
         LaunchMaximized = false;
