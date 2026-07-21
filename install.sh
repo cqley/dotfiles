ef8b5ca38a67ea808e4b8af47b6e5c599f11b1f4
caley <195605706+cqley@users.noreply.github.com>
2026-07-22 01:26:10 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,3 @@ git clone https://github.com/cqley/dotfiles "$dir"
 sudo rm -rf /etc/nixos
 sudo cp -r "$dir/nixos" /etc/
 rm -rf "$dir"
-
-sudo nixos-generate-config
-clear
-printf "host?\n"
-read -r host < /dev/tty
-
-sudo mv /etc/nixos/hardware-configuration.nix "/etc/nixos/hosts/$host/"
-sudo rm /etc/nixos/configuration.nix
