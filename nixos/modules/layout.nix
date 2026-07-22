1dc76b3b581ab4ae18ff02c1b3d15ca30d5110e4
caley <195605706+cqley@users.noreply.github.com>
2026-07-22 07:32:14 +0200

    Create layout.nix

---
+++ nixos/modules/layout.nix
@@ -0,0 +1,16 @@
+{ ... }:
+
+{
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+    download = "/home/cat/downloads";
+    documents = "/home/cat/documents";
+    pictures = "/home/cat/pictures";
+    videos = "/home/cat/videos";
+    music = "/home/cat/music";
+    desktop = "/home/cat/desktop";
+    publicShare = "/home/cat/public";
+    templates = "/home/cat/templates";
+  };
+}
