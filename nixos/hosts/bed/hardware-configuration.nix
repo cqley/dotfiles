2f1361ddb829251db4d41034763e49b62a4162a2
caley <195605706+cqley@users.noreply.github.com>
2026-07-24 03:06:08 +0200

    Update hardware-configuration.nix

--- nixos/hosts/bed/hardware-configuration.nix
+++ nixos/hosts/bed/hardware-configuration.nix
@@ -1,1 +1,28 @@
+{ config, lib, pkgs, modulesPath, ... }:
 
+{
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ "amdgpu" ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" = { 
+    device = "/dev/disk/by-uuid/e98b3077-156d-415c-bed0-a2f790ed63c6";
+    fsType = "ext4";
+    options = [ "noatime" ];
+  };
+
+  fileSystems."/boot" = { 
+    device = "/dev/disk/by-uuid/FCC2-2E30";
+    fsType = "vfat";
+    options = [ "fmask=0077" "dmask=0077" ];
+  };
+
+  swapDevices = [ ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.enableRedistributableFirmware = true;
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
