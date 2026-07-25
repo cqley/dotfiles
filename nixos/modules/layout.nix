ccc5f2341d46870ff1d342a014276132bb3498b4
caley <195605706+cqley@users.noreply.github.com>
2026-07-25 02:15:37 +0200

    Remove publicShare and templates from layout
    Removed publicShare and templates directories from layout.

--- nixos/modules/layout.nix
+++ nixos/modules/layout.nix
@@ -10,7 +10,5 @@
     videos = "/home/cat/videos";
     music = "/home/cat/music";
     desktop = "/home/cat/desktop";
-    publicShare = "/home/cat/public";
-    templates = "/home/cat/templates";
   };
 }
