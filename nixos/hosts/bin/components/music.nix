6d598880287bd56c1dc130b80f77a9044b09a0f0
caley <195605706+cqley@users.noreply.github.com>
2026-07-25 02:35:53 +0200

    Update music.nix

--- nixos/hosts/bin/components/music.nix
+++ nixos/hosts/bin/components/music.nix
@@ -1,1 +1,12 @@
-
+{
+  networking.firewall.allowedTCPPorts = [ 4533 ];
+  
+  services.navidrome = {
+    enable = true;
+    settings = {
+      Address = "0.0.0.0";
+      Port = 4533;
+      MusicFolder = "/var/lib/navidrome/music";
+    };
+  };
+}
