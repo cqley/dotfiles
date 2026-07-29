31e11d4a1e0a67784ceaf9dc567a10f05e7902dc
caley <195605706+cqley@users.noreply.github.com>
2026-07-30 01:08:49 +0200

    Update configuration.nix

--- nixos/hosts/box/configuration.nix
+++ nixos/hosts/box/configuration.nix
@@ -33,21 +33,6 @@
   
   networking.hostName = "box";
   networking.networkmanager.enable = true;
-  networking.firewall.allowedUDPPorts = [ 51820 ];
-
-  networking.wireguard.interfaces.wg0 = {
-    ips = [ "10.0.0.2/24" ];
-    privateKeyFile = "/var/lib/wireguard/privatekey";
-
-    peers = [
-      {
-        publicKey = "P2LPLBq/6qXE5Mmv8DJUviU89Yu5s7vysbyWncnZchY=";
-        allowedIPs = [ "10.0.0.0/24" ];
-        endpoint = "5.231.118.254:51820";
-        persistentKeepalive = 25;
-      }
-    ];
-  };
 
   time.timeZone = "Europe/Berlin";
   i18n.defaultLocale = "en_US.UTF-8";
