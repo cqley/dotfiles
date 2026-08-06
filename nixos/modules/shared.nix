676ed1c8f44a0957f08fe7ae7e2c9a061c7d9762
caley <195605706+cqley@users.noreply.github.com>
2026-08-06 21:04:09 +0200

    Update shared.nix

--- nixos/modules/shared.nix
+++ nixos/modules/shared.nix
@@ -10,11 +10,12 @@
       devices = {
         "box" = { id = "2M2B2UF-QRHLUFR-SFDCSFC-LU2V4BR-UAWA24J-7SNNFOK-H5WDAIV-X4AT3QY"; };
         "bed" = { id = "HYGBY4A-MQBNOTJ-HDMRQ7K-YQZK3UC-UD2KEDH-TNTZMXE-67DWKQV-UTMFOA6"; };
+        "bin" = { id = "Y2BR3CK-RUTKTKN-CNOB7J6-ZQENCK7-OQVFVWL-C3OMTNK-FXWXUPZ-H4TWPAX"; };
       };
       folders = {
         "shared" = {
           path = "/home/cat/shared";
-          devices = [ "box" "bed" ];
+          devices = [ "box" "bed" "bin" ];
         };
       };
     };
