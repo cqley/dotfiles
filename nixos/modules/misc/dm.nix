52498ff01ecc7ecc63ec802be7472b33e9e9d9ea
caley <195605706+cqley@users.noreply.github.com>
2026-08-11 21:32:09 +0200

    Create dm.nix

---
+++ nixos/modules/misc/dm.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }: {
+  services.aria2 = {
+    enable = true;
+    rpcSecretFile = "/etc/nixos/secrets/aria2";
+    settings = {
+      dir = "/home/cat/downloads";
+      rpc-allow-origin-all = true;
+      rpc-listen-all = false;
+      enable-dht = true;
+      bt-enable-lpd = true;
+      enable-peer-exchange = true;
+    };
+  };
+
+  systemd.services.aria2.serviceConfig = {
+    User = pkgs.lib.mkForce "cat";
+    Group = pkgs.lib.mkForce "users";
+    ProtectHome = pkgs.lib.mkForce false;
+    DynamicUser = pkgs.lib.mkForce false;
+  };
+
+  environment.systemPackages = with pkgs; [
+    ariang
+  ];
+}
