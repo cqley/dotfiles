ec81cb1019f005cf683df05bd135ba881d99e29d
caley <195605706+cqley@users.noreply.github.com>
2026-08-11 21:44:55 +0200

    yep

--- nixos/modules/misc/dm.nix
+++ nixos/modules/misc/dm.nix
@@ -1,4 +1,16 @@
 { pkgs, ... }: {
+  systemd.tmpfiles.rules = [
+    "d /var/lib/aria2 0755 cat users -"
+    "d /home/cat/downloads 0755 cat users -"
+  ];
+
+  environment.etc."nixos/secrets/aria2" = {
+    text = "cat";
+    mode = "0400";
+    user = "cat";
+    group = "users";
+  };
+
   services.aria2 = {
     enable = true;
     rpcSecretFile = "/etc/nixos/secrets/aria2";
