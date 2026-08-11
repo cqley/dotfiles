acdaaabe74919b53243caa2b22daa0fa94a1b75e
caley <195605706+cqley@users.noreply.github.com>
2026-08-11 22:05:46 +0200

    Update dm.nix

--- nixos/modules/misc/dm.nix
+++ nixos/modules/misc/dm.nix
@@ -1,16 +1,4 @@
 { pkgs, ... }: {
-  systemd.tmpfiles.rules = [
-    "d /var/lib/aria2 0755 cat users -"
-    "d /home/cat/downloads 0755 cat users -"
-  ];
-
-  environment.etc."nixos/secrets/aria2" = {
-    text = "cat";
-    mode = "0400";
-    user = "cat";
-    group = "users";
-  };
-
   services.aria2 = {
     enable = true;
     rpcSecretFile = "/etc/nixos/secrets/aria2";
@@ -30,8 +18,4 @@
     ProtectHome = pkgs.lib.mkForce false;
     DynamicUser = pkgs.lib.mkForce false;
   };
-
-  environment.systemPackages = with pkgs; [
-    ariang
-  ];
 }
