9d1d67592b2e49f2cecbcd7ad248f6c5cb42b647
caley <195605706+cqley@users.noreply.github.com>
2026-08-11 22:48:42 +0200

    blurppppppppp

--- nixos/modules/misc/ariang.nix
+++ nixos/modules/misc/ariang.nix
@@ -1,7 +1,12 @@
 { pkgs, ... }: {
+  systemd.tmpfiles.rules = [
+    "d /var/lib/aria2 0755 cat users -"
+    "d /home/cat/downloads 0755 cat users -"
+  ];
+
   services.aria2 = {
     enable = true;
-    rpcSecretFile = "/etc/nixos/secrets/aria2";
+    rpcSecretFile = "/var/lib/aria2/secret";
     settings = {
       dir = "/home/cat/downloads";
       rpc-allow-origin-all = true;
@@ -18,4 +23,8 @@
     ProtectHome = pkgs.lib.mkForce false;
     DynamicUser = pkgs.lib.mkForce false;
   };
+
+  environment.systemPackages = with pkgs; [
+    ariang
+  ];
 }
