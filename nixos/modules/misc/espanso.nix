4d20a465fc6869ae7ed9f5e3b0ff3f0e16662bb1
caley <195605706+cqley@users.noreply.github.com>
2026-08-26 16:09:58 +0200

    fix mistake

--- nixos/modules/misc/espanso.nix
+++ nixos/modules/misc/espanso.nix
@@ -18,6 +18,97 @@
               }
             ];
           }
+          {
+            trigger = ":github";
+            replace = "{{output}}";
+            vars = [
+              {
+                name = "output";
+                type = "shell";
+                params = {
+                  cmd = "PATH=$PATH:/run/current-system/sw/bin pass show github";
+                };
+              }
+            ];
+          }
+          {
+            trigger = ":codeberg";
+            replace = "{{output}}";
+            vars = [
+              {
+                name = "output";
+                type = "shell";
+                params = {
+                  cmd = "PATH=$PATH:/run/current-system/sw/bin pass show codeberg";
+                };
+              }
+            ];
+          }
+          {
+            trigger = ":reddit";
+            replace = "{{output}}";
+            vars = [
+              {
+                name = "output";
+                type = "shell";
+                params = {
+                  cmd = "PATH=$PATH:/run/current-system/sw/bin pass show reddit";
+                };
+              }
+            ];
+          }
+          {
+            trigger = ":discord";
+            replace = "{{output}}";
+            vars = [
+              {
+                name = "output";
+                type = "shell";
+                params = {
+                  cmd = "PATH=$PATH:/run/current-system/sw/bin pass show discord";
+                };
+              }
+            ];
+          }
+          {
+            trigger = ":icloud";
+            replace = "{{output}}";
+            vars = [
+              {
+                name = "output";
+                type = "shell";
+                params = {
+                  cmd = "PATH=$PATH:/run/current-system/sw/bin pass show icloud";
+                };
+              }
+            ];
+          }
+          {
+            trigger = ":hytale";
+            replace = "{{output}}";
+            vars = [
+              {
+                name = "output";
+                type = "shell";
+                params = {
+                  cmd = "PATH=$PATH:/run/current-system/sw/bin pass show hytale";
+                };
+              }
+            ];
+          }
+          {
+            trigger = ":skrime";
+            replace = "{{output}}";
+            vars = [
+              {
+                name = "output";
+                type = "shell";
+                params = {
+                  cmd = "PATH=$PATH:/run/current-system/sw/bin pass show skrime";
+                };
+              }
+            ];
+          }
         ];
       };
     };
