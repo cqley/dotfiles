f408a124e9cb0accd88f0b76accc4928f90c24af
caley <195605706+cqley@users.noreply.github.com>
2026-09-22 14:54:39 +0200

    Update filehost.nix

--- nixos/hosts/bin/components/filehost.nix
+++ nixos/hosts/bin/components/filehost.nix
@@ -4,11 +4,10 @@
     wantedBy = [ "multi-user.target" ];
     after = [ "network.target" ];
     serviceConfig = {
-      ExecStart = "${pkgs.linx-server}/bin/linx-server -bind 127.0.0.1:47291 -siteurl https://host.1f2t.org/ ...";
+      ExecStart = "${pkgs.linx-server}/bin/linx-server -bind 127.0.0.1:47291 -siteurl https://host.1f2t.org/ -filespath /var/lib/linx-server/files -metapath /var/lib/linx-server/meta";
+      WorkingDirectory = "/var/lib/linx-server";
+      StateDirectory = "linx-server";
       DynamicUser = true;
-      StateDirectory = "linx-server linx-server/files linx-server/meta";
     };
   };
-
-  networking.firewall.allowedTCPPorts = [ 47291 ];
 }
